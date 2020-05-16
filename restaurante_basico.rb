b24b8a5ab10652b002e68b643bc725dab94227f3
klebermd13 <62228769+klebermd13@users.noreply.github.com>
2020-05-16 19:17:32 -0300

    nota

--- restaurante_basico.rb
+++ restaurante_basico.rb
@@ -1,3 +1,10 @@
 puts "Digite o nome do restaurante: "
 nome = gets 
-print "Nome do restaurante: #{nome.capitalize} "
+print "Nome do restaurante: #{nome.capitalize!} "
+
+puts "Digite a nota do restaurante: "
+nota = gets
+print " O restaurante #{nome} teve a nota #{nota}"
+
+
+
